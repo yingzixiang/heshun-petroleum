a4ec76d01e580e858c1a756300105e5247678ce1
zixiang Ying <15168316032@163.com>
2017-06-01 19:17:39 +0800

    和顺石油 80%

---
+++ js/ui.js
@@ -0,0 +1,9 @@
+	//页面中添加尾部节点
+	$(function(){
+		var htmlFooter = "";
+		htmlFooter += '<div class="weui-footer member-info-footer">'
+		htmlFooter += 	'<p class="weui-footer__text">和顺石油移动互联网+会员</p>'
+		htmlFooter +=   '<p class="weui-footer__text">技术支持 © 极速挪车TM jisunuoche.com</p>'
+		htmlFooter += '</div>'
+		$("body").append(htmlFooter)
+	})
